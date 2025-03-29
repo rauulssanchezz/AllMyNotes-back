f68d5f222210435941801313ab972e5240b3d72c
Raúl Sánchez <132095151+rauulssanchezz@users.noreply.github.com>
2025-03-29 18:50:34 +0100

    FEAT: use BcryptAdapter to hash user password

--- src/presentation/auth/controllert.ts
+++ src/presentation/auth/controllert.ts
@@ -1,17 +1,20 @@
 
-import express from 'express';
-import { RegisterUserDto } from '../../domain/dto/register-user.dto.ts';
+import express from 'express'
+import { RegisterUserDto } from '../../domain/dto/register-user.dto.ts'
+import { BcryptAdapter } from '../../config/index.ts';
 
 
 export class AuthController {
 
-    registerUser = (req: express.Request, res: express.Response) => {
+    registerUser = async (req: express.Request, res: express.Response) => {
         const { name, email, password } = req.body;
 
+        const hashPassword = await BcryptAdapter.hash(password)
+
         const newUser = RegisterUserDto.create({
-            name,
-            email,
-            password
+            name: name,
+            email: email,
+            password: hashPassword
         })
 
         res.json({
