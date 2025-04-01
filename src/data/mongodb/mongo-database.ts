71981d1d9979a88a92cf7ee152099cf9c97cc041
Jorge <jorgemartinherrera2004@gmail.com>
2025-04-01 23:17:38 +0200

    tsconfig.json hecho(habia que poner skiplibcheck para que en el mongoose.connect() no te pidiera más cosas) , instalados dotenv, env-var y mongoose en el package.json, hecho el envs.ts y el mongo-database.ts

---
+++ src/data/mongodb/mongo-database.ts
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+interface Options {
+    mongoUrl: string,
+    dbName: string,
+    options?: mongoose.ConnectOptions;
+}
+
+export class MongoDatabase {
+
+    static async connect(options: Options) {
+
+        const { dbName, mongoUrl } = options
+
+        try {
+
+            await mongoose.connect(mongoUrl, {
+                dbName: dbName,
+            })
+
+            console.log('Mongo connected')
+            return true
+
+        } catch(error) {
+
+            console.error('Error connecting to MongoDB:', error);
+            throw error;
+
+        }
+    }
+
+}
